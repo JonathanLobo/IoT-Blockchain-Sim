d8f8f3d9da94302ca173ceeed842730018ffaf14
Chris C <chc200@pitt.edu>
2018-04-25 14:37:35 -0400

    posstart

---
+++ pos/MainServer.py
@@ -0,0 +1,108 @@
+import socket
+import sys
+import struct
+from Block import Block
+from BlockChain import BlockChain
+from random import *
+
+def send_msg(sock, msg):
+    # Prefix each message with a 4-byte length (network byte order)
+    msg = struct.pack('>I', len(msg)) + msg
+    sock.sendall(msg)
+
+def recv_msg(sock):
+    # Read message length and unpack it into an integer
+    raw_msglen = recvall(sock, 4)
+    if not raw_msglen:
+        return None
+    msglen = struct.unpack('>I', raw_msglen)[0]
+    # Read the message data
+    return recvall(sock, msglen)
+
+def recvall(sock, n):
+    # Helper function to recv n bytes or return None if EOF is hit
+    data = b''
+    while len(data) < n:
+        packet = sock.recv(n - len(data))
+        if not packet:
+            return None
+        data += packet
+    return data
+
+# Create BlockChain
+
+bc = BlockChain(5,"hello world")
+bc.AddBlockServer(Block(1, "Block 1 Data"))
+
+# Create a TCP/IP socket
+sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+# Bind the socket to the port
+server_address = ('', 8000)
+print('Starting up on port %s' % server_address[1])
+sock.bind(server_address)
+
+sock.listen(4)
+
+transactions = 1
+
+while True:
+    # Wait for a connection
+    print('waiting for a connection')
+    connection, client_address = sock.accept()
+
+    print('connection from' + str(client_address))
+
+    # Receive the data
+    while True:
+
+        data = recv_msg(connection)
+        if data:
+            data = data.decode("utf-8").split(',')
+            message = data[0]
+            print('received ' + message)
+            if (message == 'new'):
+                chain = bc.getChain()
+                chainString = str(transactions) + ','
+
+                for i in range(0, len(chain)):
+                    if (i == 0):
+                        chainString = chainString + str(bc.getDifficulty()) + ';' + str(chain[i].getSData())
+                    else:
+                        chainString = chainString + chain[i].getData()
+                    if (i != len(chain) - 1):
+                        chainString = chainString + ','
+
+                send_msg(connection, chainString.encode())
+                print('sent the blockchain to ' + str(client_address))
+
+            elif (message == 'NEWBLOCK'):
+                # think about edge case if 2 nodes send newblock at once
+                vals = data[1].split(';')
+                bNew = Block(vals[0], vals[1], vals[2], vals[3], vals[4], vals[5])
+                bc.AddBlock1(bNew)
+                transactions = str(randint(1, 100000))
+                send_msg(connection, str(transactions).encode())
+
+            elif (message == 'DIDILOSE'):
+                if (int(data[1]) == len(bc.getChain())):
+                    send_msg(connection, 'NO'.encode())
+                else:
+                    send_msg(connection, 'YES'.encode())
+
+            elif (message == 'UPDATEME'):
+                chainString = str(transactions) + ','
+                index = int(data[1]) + 1
+                chain = bc.getChain()
+
+                for i in range(index, len(chain)):
+                    chainString = chainString + chain[i].getData()
+                    if i != len(chain) - 1:
+                        chainString = chainString + ','
+
+                send_msg(connection, chainString.encode())
+
+            break
+
+# Clean up the connection
+connection.close()
---
+++ pos/mainClient.py
@@ -0,0 +1,132 @@
+import socket
+import sys
+import struct
+import time
+
+
+from Block import Block
+from BlockChain import BlockChain
+
+start = 0
+ip = "192.168.1.154"
+port = 8000
+
+def send_msg(sock, msg):
+	# Prefix each message with a 4-byte length (network byte order)
+	msg = struct.pack('>I', len(msg)) + msg
+	sock.sendall(msg)
+
+def recv_msg(sock):
+	# Read message length and unpack it into an integer
+	raw_msglen = recvall(sock, 4)
+	if not raw_msglen:
+		return None
+	msglen = struct.unpack('>I', raw_msglen)[0]
+	# Read the message data
+	return recvall(sock, msglen)
+
+def recvall(sock, n):
+	# Helper function to recv n bytes or return None if EOF is hit
+	data = b''
+	while len(data) < n:
+		packet = sock.recv(n - len(data))
+		if not packet:
+			return None
+		data += packet
+	return data
+
+# Create a TCP/IP socket
+sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+# Connect the socket to the port where the server is listening
+server_address = (ip, port)
+print('connecting to %s port %s' % server_address)
+connection = sock.connect(server_address)
+
+
+bc = ""
+
+mine = True
+myDataToMine = -1
+
+while True:
+	if(start == 0):
+		message = 'new'
+		send_msg(sock,message.encode())
+		start = 1
+		#sock.sendall(message.encode())
+	if(start == 1):
+		start = 2
+		
+		data = recv_msg(sock).decode()
+
+		blocks = data.split(",")
+
+		for block in range(0,len(blocks)):
+			vals = blocks[block].split(";")
+
+			if(block == 0):
+				myDataToMine = vals[0]
+
+			elif(block == 1):
+				bc = BlockChain(vals[0],vals[1])
+			else:
+				print(vals)
+				bNew = Block(int(vals[0]), vals[1], int(vals[2]), vals[3], vals[4], vals[5])
+				err = bc.AddBlock1(bNew)
+		sock.close()
+	else:
+		#print("DONE")
+		if(mine):
+			mine = False
+
+			block = Block(len(bc.getChain()), myDataToMine)
+			finished = bc.AddBlock(block)
+
+			if finished == 1:
+				sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+				sock.connect(server_address)
+				
+				message = 'NEWBLOCK,'
+				block = block.getData()
+				
+				message = message + block
+				send_msg(sock,message.encode())
+				myDataToMine = recv_msg(sock).decode()
+				mine=True
+		
+			elif finished == -1:
+				sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+				sock.connect(server_address)
+
+				message = 'UPDATEME,' + str(len(bc.getChain())-1)
+				send_msg(sock,message.encode())
+				
+				data = recv_msg(sock).decode()
+				
+				blocks = data.split(",")
+
+				for block in range(0,len(blocks)):
+					if(block == 0):
+						myDataToMine = vals[0]
+					else:
+						vals = blocks[block].split(";")
+						bNew = Block(vals[0], vals[1], vals[2], vals[3], vals[4], vals[5])
+						err = bc.AddBlock1(bNew)
+
+				mine = True
+			sock.close()
+
+		c = bc.getChain()
+
+		for i in c:
+			print(i.getData())
+		
+
+
+
+
+	
+
+
+
